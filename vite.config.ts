9b44c8765c0fd293a1b08ce82d0c43c67f08e748
Eslam-muhamed <jrsllam@gmail.com>
2026-07-28 07:52:25 +0300

    edit vital

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/menu_NEW/", // 👈 أضف السطر ده
+  base: "/", // 👈 أضف السطر ده
 
   server: {
     host: "::",
